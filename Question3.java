d7390676b812c85a9e43325ad5cb16c5eb270777
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-07 14:09:51 -0400

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question3 {
-    public static void main(String[] args) {
+    System.out.println("Input The Radius");
+    double x = scan.nextDouble();
+    double circ = (2 * 3.14) * x;
+    double area = (x * x) * 3.14;
+    System.out.println("Circumference: " + circ);
+    System.out.println("Area: " + area);
+    
 
-        /* Write your code here */
-
-    }
+  }
 }

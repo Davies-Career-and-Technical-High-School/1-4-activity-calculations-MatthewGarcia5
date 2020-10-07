33751cf4671d8ff49660f0e34f92348095013958
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-07 14:13:13 -0400

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question4 {
-    public static void main(String[] args) {
+    System.out.println("Enter A Price");
+    double x = scan.nextDouble();
+    double change = 20 - x; 
+    System.out.println("Change From $20: $" + change);
+    
 
-        /* Write your code here */
-
-    }
+  }
 }

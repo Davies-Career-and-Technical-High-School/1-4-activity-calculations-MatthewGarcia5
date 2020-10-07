b214c86485ebd5478ceb115e4cd00639b9f2142b
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-07 14:01:23 -0400

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question1 {
-    public static void main(String[] args) {
+    System.out.println("Choose an Integer");
+    int x = scan.nextInt();
 
-        /* Write your code here */
-
+    for(int i = x; i < x + 4; i++) {
+      System.out.println("The number is now " + i);
     }
+
+  }
 }
